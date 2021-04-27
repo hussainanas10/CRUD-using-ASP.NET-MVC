34f06dc02a5cd7a62b2320158868818c1850c06e
Anas Hussain <anashussain1999@gmail.com>
2021-04-27 11:29:25 +0530

    commit 1

---
+++ demo/Controllers/HomeController.cs
@@ -0,0 +1,65 @@
+﻿using demo.Model;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.Validation;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace demo.Controllers
+{
+    public class HomeController : Controller
+    {
+        STUDENTSEntities2 obj = new STUDENTSEntities2();
+
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult addEmployees()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult AddEmployee(Employe_de model)
+        {
+            Employe_de data = new Employe_de();
+            data.EmployeID = model.EmployeID;
+            data.Name = model.Name;
+            data.Salary = model.Salary;
+            data.Grade = model.Grade;
+
+            obj.Employe_de.Add(data);
+            obj.SaveChanges();
+
+            ModelState.Clear();
+
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Display()
+        {
+            var res = obj.Employe_de.ToList();
+            return View();
+        }
+
+        public ActionResult newdisplay()
+        {
+            var dis = obj.Employe_de.ToList<Employe_de>();
+            return Json(new { data = dis }, JsonRequestBehavior.AllowGet);
+        }
+       public ActionResult update_info()
+        {
+            return View();
+        }
+        public ActionResult Delete(int id)
+        {
+            var res = obj.Employe_de.Where(x => x.ID == id).First();
+            obj.Employe_de.Remove(res);
+            obj.SaveChanges();
+            //var res = obj.Employe_de.ToList();
+            return Redirect("Display");
+        }
+    }
+}
